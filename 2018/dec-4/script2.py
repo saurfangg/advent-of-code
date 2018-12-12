ba466764ac1371ea6ffcb99749124f32af3b5bf7
saurfangg <goran.osim@gmail.com>
2018-12-12 15:37:34 -0800

    Day 4 Solution and moved all solutions into a year folder.

---
+++ 2018/dec-4/script2.py
@@ -0,0 +1,23 @@
+import re
+from collections import Counter
+
+lines = [line.strip() for line in open('input.txt')]
+lines.sort()
+
+guards = {}
+
+current_guard, start_time, end_time = 0, 0, 0
+
+for line in lines:
+    value = re.findall("\d+", line)
+    if "Guard" in line:
+        current_guard = int(value[-1])
+    elif "falls asleep" in line:
+        start_time = int(value[-1])
+    elif "wakes up" in line:
+        end_time = int(value[-1])
+        for i in range(start_time, end_time):
+            guards.setdefault(current_guard, []).append(i)
+
+guard_id = max(guards, key=lambda x: Counter(guards[x]).most_common()[0][1])
+print(guard_id * Counter(guards[guard_id]).most_common()[0][0])
